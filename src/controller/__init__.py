f14d1f34999d181d60ad2c1979c716fbac359132
Jiezhoue <13809@coderacademy.edu.au>
2023-03-01 15:01:50 +1100

    each patient can login and check their own personal detail and bookings under their name

--- src/controller/__init__.py
+++ src/controller/__init__.py
@@ -1,10 +1,12 @@
 from controller.home_controller import home
 from controller.dentist_controller import dentist
 from controller.auth_controller import auth
+from controller.user_controller import patient
 
 
 registerable_controllers =[
     home,
     dentist,
-    auth
+    auth,
+    patient
 ]
---
+++ src/controller/user_controller.py
@@ -0,0 +1,32 @@
+from flask import jsonify, Blueprint, request, abort
+from main import db
+from models.users import User
+from schema.user_schema import user_schema, users_schema
+from main import bcrypt
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+
+patient = Blueprint('patient', __name__, url_prefix='/patient')
+
+@patient.get("/info")
+@jwt_required()
+def patient_info():
+    user_name = get_jwt_identity()
+    user = User.query.filter_by(username=user_name).first()
+    if not user:
+        return abort(401, description="Invaild user")
+    
+    result = user_schema.dump(user)
+    return jsonify(result)
+
+@patient.post("/booking")
+@jwt_required()
+
+
+
+
+
+
+    
+
