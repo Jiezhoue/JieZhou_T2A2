e2190917b9df6ee74449a2d173a86437c3e90c2c
Jiezhoue <13809@coderacademy.edu.au>
2023-03-01 12:32:10 +1100

    create signup, cause mobile is nullable, check mobile key value first when adding user info

--- src/controller/auth_controller.py
+++ src/controller/auth_controller.py
@@ -14,19 +14,47 @@ def get_user():
     result = users_schema.dump(users)
     return jsonify(result)
 
+
 @auth.post("/login")
 def user_login():
     try:
-        user_field = user_schema.load(request.json)
-        user = User.query.filter_by(username=user_field["username"]).first()
+        user_fields = user_schema.load(request.json)
+        user = User.query.filter_by(username=user_fields["username"]).first()
         if not user:
             return abort(401, description="username is not exist")
-        elif not bcrypt.check_password_hash(user.password, user_field["password"]):
+        elif not bcrypt.check_password_hash(user.password, user_fields["password"]):
             return abort(401, description="password is not right")
         access_token = create_access_token(identity=str(user.username))
         return jsonify({"user": user.username, "token": access_token})
     except ValidationError:
         return abort(401, description="minimun password length is 8")
+
+
+@auth.post("/signup")
+def signup():
+    try:
+        user_fields = user_schema.load(request.json)
+        user = User.query.filter_by(username=user_fields["username"]).first()
+        if user:
+            return abort(401, description="Username is already registered. Please choose another username.")
+
+        user = User()
+        user.f_name = user_fields["f_name"]
+        user.l_name = user_fields["l_name"]
+
+        if "mobile" in user_fields:
+            user.mobile = user_fields["mobile"]
+        user.username = user_fields["username"]
+        user.password = bcrypt.generate_password_hash(user_fields["password"]).decode("utf-8")
+
+        db.session.add(user)
+        db.session.commit()
+
+        access_token = create_access_token(identity=str(user.username))
+        return jsonify({"user": user.username, "token":access_token})
+    except ValidationError:
+        return abort(401, description="minimun password length is 8")
+
 
 
 
